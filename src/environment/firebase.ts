96a9a6242748b2bd90251793bf4707f44c87d405
pawelczak <lukaspawelczak@gmail.com>
2017-05-13 21:00:23 +0200

    Updated gitingore

--- src/environment/firebase.ts
+++ src/environment/firebase.ts
@@ -1,8 +1,8 @@
 export const firebase = {
-    apiKey: '<your-key>',
-    authDomain: '<your-project-authdomain>',
-    databaseURL: '<your-database-URL>',
-    projectId: '<your-project-id>',
-    storageBucket: '<your-storage-bucket>',
-    messagingSenderId: '<your-messaging-sender-id>'
+    // apiKey: '<your-key>',
+    // authDomain: '<your-project-authdomain>',
+    // databaseURL: '<your-database-URL>',
+    // projectId: '<your-project-id>',
+    // storageBucket: '<your-storage-bucket>',
+    // messagingSenderId: '<your-messaging-sender-id>'
 };
